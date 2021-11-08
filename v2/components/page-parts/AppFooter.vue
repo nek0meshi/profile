0e16427e1d7997b4bb31091ca755c1a069b601fa
nek0meshi <nekmeshi@gmail.com>
2021-11-08 22:45:56 +0900

    - footer

--- v2/components/page-parts/AppFooter.vue
+++ v2/components/page-parts/AppFooter.vue
@@ -1,17 +1,71 @@
 <template>
   <footer>
+    <ul class="logos flex flex-row">
+      <li
+        v-for="link in links"
+        :key="link.title"
+      >
+        <a :href="link.url" target="_blank">
+          <img
+            :src="'logo/' + link.imageFileName"
+            class="link-logo"
+            :style="{ 'background-color': link.bdColor || '#fff' }"
+          >
+        </a>
+      </li>
+    </ul>
     <p>©︎ 2021 YUSHI WATANABE</p>
   </footer>
 </template>
+
+<script>
+export default {
+  computed: {
+    links() {
+      return [
+        {
+          title: 'github',
+          url: 'https://github.com/nek0meshi',
+          imageFileName: 'GitHub-Mark-Light-120px-plus.png',
+          bdColor: '#000',
+        },
+        {
+          title: 'twitter',
+          url: 'https://twitter.com/yushi0dev',
+          imageFileName: 'Twitter social icons - circle - blue.png',
+        },
+        {
+          title: 'hatena-blog',
+          url: 'https://yushi-dev.hatenablog.com/',
+          imageFileName: 'hatenablog-logo.svg',
+        },
+      ]
+    }
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 footer {
   display: flex;
   flex-direction: column;
   justify-content: center;
-  widows: 100%;
-  height: 150px;
+  align-items: center;
+  gap: 20px;
+  height: 180px;
+  width: 100%;
   background-color: $main-1;
   color: #fff;
+}
+
+.logos {
+  gap: 30px;
+}
+
+.link-logo {
+  background-color: #fff;
+  height: 50px;
+  width: 50px;
+  border-radius: 100%;
 }
 </style>

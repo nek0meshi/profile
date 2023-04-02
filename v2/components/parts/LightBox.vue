b4e6fd1c00380cb0e5e70ba3f4299b5601949aa9
nek0meshi <nekmeshi@gmail.com>
2023-04-02 22:29:17 +0900

    LightBoxに左右の移動ボタンを追加

--- v2/components/parts/LightBox.vue
+++ v2/components/parts/LightBox.vue
@@ -1,11 +1,21 @@
 <template>
   <transition name="fade">
     <div
-      v-if="filePath"
-      class="light-box flex justify-center items-center"
+      v-show="filePath"
+      class="light-box flex flex-row justify-around items-center"
       @click="$emit('close')"
     >
-      <img class="image" :src="filePath" />
+      <button
+        class="arrow-button left-arrow"
+        :class="{ hasNext: hasLeft }"
+        @click.stop="$emit('go-left')"
+      />
+      <img class="image" :src="filePath" @click.stop />
+      <button
+        class="arrow-button right-arrow"
+        :class="{ hasNext: hasRight }"
+        @click.stop="$emit('go-right')"
+      />
     </div>
   </transition>
 </template>
@@ -16,6 +26,14 @@ export default {
     filePath: {
       type: String,
       default: '',
+    },
+    hasLeft: {
+      type: Boolean,
+      default: false,
+    },
+    hasRight: {
+      type: Boolean,
+      default: false,
     },
   },
 }
@@ -33,9 +51,68 @@ export default {
   cursor: pointer;
 }
 .image {
-  width: 90%;
-  height: 90%;
+  max-width: calc(100% - 150px);
+  max-height: 90%;
   object-fit: contain;
+
+  @media (max-width: $mobile-max-width) {
+    max-width: 100%;
+  }
+}
+$arrow-color: #aaa;
+$arrow-size: 15px;
+$arrow-btn-size: 50px;
+.arrow-button {
+  position: relative;
+  background-color: #333;
+  width: $arrow-btn-size;
+  height: $arrow-btn-size;
+  border-radius: 50%;
+  opacity: 0;
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: calc($arrow-btn-size / 2 - $arrow-size);
+    border-top: $arrow-size solid transparent;
+    border-bottom: $arrow-size solid transparent;
+  }
+  &.left-arrow::after {
+    border-right: $arrow-size solid $arrow-color;
+    left: calc($arrow-btn-size / 2 - $arrow-size * 2 / 3);
+  }
+  &.right-arrow::after {
+    border-left: $arrow-size solid $arrow-color;
+    right: calc($arrow-btn-size / 2 - $arrow-size * 2 / 3);
+  }
+
+  &.hasNext {
+    opacity: 0.5;
+  }
+
+  @media (max-width: $mobile-max-width) {
+    position: fixed;
+    top: 0;
+    width: 100px;
+    height: 100%;
+    border-radius: 0;
+
+    &::after {
+      display: none;
+    }
+
+    &.left-arrow {
+      left: 0;
+    }
+
+    &.right-arrow {
+      right: 0;
+    }
+
+    &.hasNext {
+      opacity: 0;
+    }
+  }
 }
 .fade-enter-active {
   transition: all 0.5s ease;

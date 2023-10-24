b57cd41bfc74a20f06ea0cc826175bbc32930058
nek0meshi <nekmeshi@gmail.com>
2023-10-24 17:13:45 +0900

    画面表示のための転記と細かい修正

--- v3/app.vue
+++ v3/app.vue
@@ -1,5 +1,45 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <AppHeader />
+    <Hero />
+    <AboutSection />
+    <HistorySection />
+    <SkillsSection />
+    <LinksSection />
+    <PhotosSection />
+    <AppFooter />
   </div>
 </template>
+
+<script>
+import AppFooter from './components/page-parts/AppFooter.vue'
+import AppHeader from './components/page-parts/AppHeader.vue'
+import AboutSection from './components/page-parts/AboutSection.vue'
+import Hero from './components/page-parts/Hero.vue'
+import HistorySection from './components/page-parts/HistorySection.vue'
+import SkillsSection from './components/page-parts/SkillsSection.vue'
+import LinksSection from './components/page-parts/LinksSection.vue'
+import PhotosSection from './components/page-parts/PhotosSection.vue'
+
+export default {
+  components: {
+    AppFooter,
+    AppHeader,
+    AboutSection,
+    Hero,
+    HistorySection,
+    SkillsSection,
+    LinksSection,
+    PhotosSection,
+  },
+}
+</script>
+
+<style lang="scss">
+@use '~/assets/scss/main.scss';
+
+section {
+  padding-top: 70px;
+  padding-bottom: 70px;
+}
+</style>
--- v3/components/page-parts/Hero.vue
+++ v3/components/page-parts/Hero.vue
@@ -10,7 +10,7 @@
 .hero-image {
   width: 100%;
   height: 100vh;
-  background-image: url('./static/hero.jpg');
+  background-image: url('./hero.jpg');
   h1 {
     color: #fff;
     font-size: 72px;

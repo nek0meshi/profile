dd7747df3a9a320dab7e1566897e3ec3a9051d63
nek0meshi <nekmeshi@gmail.com>
2024-02-12 00:04:40 +0900

    内容更新

--- v3/components/page-parts/AboutSection.vue
+++ v3/components/page-parts/AboutSection.vue
@@ -6,8 +6,7 @@ import AppContainer from '~/components/parts/AppContainer.vue'
   <app-container id="about" title="ABOUT">
     <div>
       <p>2017年より、関西でWebエンジニアをしています。</p>
-      <p>PHP、JavaScriptをよく書いています。</p>
-      <p>広く浅くかじるタイプです。</p>
+      <p>PHP、JavaScriptでの開発経験が多いです。</p>
       <p>
         趣味は、ギター・ベース、音楽・漫画・アニメ鑑賞、将棋、卓球、ルービックキューブ、写真などです。
       </p>
